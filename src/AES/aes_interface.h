384711a96c75082272c41e0a6c3a89268dcec610
Mihai Mocanu <81875967+MihaiMocanuGit@users.noreply.github.com>
2025-02-08 13:16:31 +0200

    Updated to change references of Victim to Doppelganger

--- src/AES/aes_interface.h
+++ src/AES/aes_interface.h
@@ -1,5 +1,5 @@
-#ifndef CRYPTOLYSER_VICTIM_AES_AES_H_
-#define CRYPTOLYSER_VICTIM_AES_AES_H_
+#ifndef CRYPTOLYSER_DOPPELGANGER_AES_AES_H_
+#define CRYPTOLYSER_DOPPELGANGER_AES_AES_H_
 
 #include <stdint.h>
 #include <stdio.h>
@@ -32,4 +32,4 @@ void aes_clean(struct aes_ctx_t *ctx);
 
 void aes_log_status(FILE *stream);
 
-#endif // CRYPTOLYSER_VICTIM_AES_AES_H_
+#endif // CRYPTOLYSER_DOPPELGANGER_AES_AES_H_
